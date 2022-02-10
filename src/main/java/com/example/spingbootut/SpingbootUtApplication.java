ec968a99b2e620b0c1b39aa0a0a1f2cfb8c00433
wchen <winston.chen@activenetwork.com>
2022-02-10 15:56:54 +0800

    init

---
+++ src/main/java/com/example/spingbootut/SpingbootUtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingbootut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootUtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootUtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spingbootut/SpingbootUtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingbootut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootUtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
